b2531c355b74efc673a831e956e73a8936057c75
devrimalpoge@gmail.com <devrimalpoge@gmail.com>
2025-11-04 14:57:45 +0000

    Financial KPI (average basket, turnover, etc.) on a daily basis

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,15 @@
+SELECT
+    date_date,
+    COUNT(orders_id) AS total_transactions,
+    ROUND(SUM(revenue), 2) AS revenue,
+    ROUND(SUM(revenue/quantity), 2) AS average_basket,
+    ROUND(SUM(operational_margin), 2) AS operational_margin,
+    ROUND(SUM(purchase_cost), 2) AS purchase_cost,
+    ROUND(SUM(logcost), 2) AS log_costs,
+    ROUND(SUM(shipping_fee), 2) AS shipping_fee,
+    ROUND(SUM(quantity), 2) AS products_sold
+FROM
+    {{ ref('int_orders_operational') }}
+
+GROUP BY
+    date_date
