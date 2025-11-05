61b9cf34698186056397047d8df8dcdfb550c0a2
devrimalpoge@gmail.com <devrimalpoge@gmail.com>
2025-11-05 14:37:30 +0000

    intermediate commit

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,18 @@
+SELECT
+    cd.date_date,
+    (fd.operational_margin - cd.ads_cost) AS ads_margin,
+    fd.operational_margin,
+    cd.ads_cost,
+    cd.impression AS ads_impression,
+    cd.click AS ads_click,
+    fd.products_sold AS quantity,
+    fd.revenue,
+    fd.purchase_cost,
+    fd.shipping_fee,
+    fd.log_costs
+FROM
+    {{ ref('int_campaign_day') }} cd
+LEFT JOIN
+    {{ ref('finance_days') }} fd
+USING
+    (date_date)
